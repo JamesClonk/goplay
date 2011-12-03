5e47a21b16670c1791fcb9666e96391456c364f8
John Mac <charles.beck@sent.com>
2011-12-03 17:08:24 +0000

    Removed unnecessary functions

--- gonow/gonow.go
+++ gonow/gonow.go
@@ -28,8 +28,6 @@ import (
 const SUBDIR = ".go" // To install compiled programs
 
 var (
-	file *os.File // The Go file
-
 	//interpreter    = []byte("#!/usr/bin/gonow")
 	interpreterEnv = []byte("#!/usr/bin/env gonow")
 )
@@ -64,10 +62,10 @@ func main() {
 	}
 	// ===
 
-	// Go variables
-	env := getEnv()
+	env := getEnv() // Go variables
 
 	// === Paths
+	// ===
 	scriptPath := flag.Args()[0]
 	scriptDir, scriptName := filepath.Split(scriptPath)
 	ext := filepath.Ext(scriptName)
@@ -80,27 +78,27 @@ func main() {
 			fatalf("Could not get absolute path: %s\n", err)
 		}
 
-		binaryDir = filepath.Join(env.gopath, "pkg",
-			runtime.GOOS+"_"+runtime.GOARCH, SUBDIR,
-			hash(scriptDirAbs))
-		// Local directory
+		binaryDir = filepath.Join(
+			env.gopath,
+			"pkg",
+			runtime.GOOS+"_"+runtime.GOARCH,
+			SUBDIR,
+			hash(scriptDirAbs),
+		)
 	} else {
-		if scriptDir == "" {
-			scriptDir = "./"
-		}
-
+		// Local directory
 		// Work in shared filesystems
-		binaryDir = filepath.Join(scriptDir, SUBDIR,
-			runtime.GOOS+"_"+runtime.GOARCH)
+		binaryDir = filepath.Join(SUBDIR, runtime.GOOS+"_"+runtime.GOARCH)
 	}
 
 	binaryPath = filepath.Join(binaryDir, strings.Replace(scriptName, ext, "", 1))
 
-	// Windows doesn't like running binaries without the .exe extension
+	// Windows doesn't like running binaries without the ".exe" extension
 	if runtime.GOOS == "windows" {
 		binaryPath += ".exe"
 	}
-	// ===
+
+	// * * *
 
 	// Check directory
 	if ok := exist(binaryDir); !ok {
@@ -111,34 +109,40 @@ func main() {
 
 	scriptMtime := getTime(scriptPath)
 
-	// Run the executable, if exist and it has not been modified
+	// === Run and exit
+	// If the executable exist and it has not been modified
 	if !*force && exist(binaryPath) {
 		binaryMtime := getTime(binaryPath)
 
 		if scriptMtime.Equal(binaryMtime) || scriptMtime.Before(binaryMtime) {
-			run(binaryPath)
+			runAndExit(binaryPath)
 		}
 	}
 
-	file = openFile(scriptPath)
+	// === Compile and link
+	// ===
+	file, err := os.OpenFile(scriptPath, os.O_RDWR, 0)
+	if err != nil {
+		fatalf("Could not open file: %s\n", err)
+	}
 	defer file.Close()
 
-	// === Compile and link
-	archChar, err := build.ArchChar(runtime.GOARCH)
-	if err != nil {
-		fatalf("%s", err)
-	}
-
-	objectPath := filepath.Join(binaryDir, "_go_."+archChar)
-	compiler := filepath.Join(env.gobin, archChar+"g")
-	linker := filepath.Join(env.gobin, archChar+"l")
-
-	// Compile source file
 	hasInterpreter := checkInterpreter(file)
 	if hasInterpreter {
 		comment(file)
 	}
 
+	// === Set toolchain
+	archChar, err := build.ArchChar(runtime.GOARCH)
+	if err != nil {
+		fatalf("%s", err)
+	}
+
+	compiler := filepath.Join(env.gobin, archChar+"g")
+	linker := filepath.Join(env.gobin, archChar+"l")
+
+	// === Compile source file
+	objectPath := filepath.Join(binaryDir, "_go_."+archChar)
 	cmd := exec.Command(compiler, "-o", objectPath, scriptPath)
 	out, err := cmd.CombinedOutput()
 
@@ -149,39 +153,39 @@ func main() {
 		fatalf("%s\n%s", cmd.Args, out)
 	}
 
-	// Link executable
+	// === Link executable
 	out, err = exec.Command(linker, "-o", binaryPath, objectPath).
 		CombinedOutput()
 	if err != nil {
 		fatalf("Linker failed: %s\n%s", err, out)
 	}
 
-	// Cleaning
+	// === Cleaning
 	if err := os.Remove(objectPath); err != nil {
 		fatalf("Could not remove object file: %s\n", err)
 	}
 
 	// Run executable
-	run(binaryPath)
+	runAndExit(binaryPath)
 }
 
 // === Utility
 // ===
 
 // Comments the line interpreter.
-func comment(fd *os.File) {
-	file.Seek(0, 0)
+func comment(f *os.File) {
+	f.Seek(0, 0)
 
-	if _, err := fd.Write([]byte("//")); err != nil {
+	if _, err := f.Write([]byte("//")); err != nil {
 		fatalf("Could not comment the line interpreter: %s\n", err)
 	}
 }
 
 // Comments out the line interpreter.
-func commentOut(fd *os.File) {
-	file.Seek(0, 0)
+func commentOut(f *os.File) {
+	f.Seek(0, 0)
 
-	if _, err := fd.Write([]byte("#!")); err != nil {
+	if _, err := f.Write([]byte("#!")); err != nil {
 		fatalf("Could not comment out the line interpreter: %s\n", err)
 	}
 }
@@ -189,8 +193,8 @@ func commentOut(fd *os.File) {
 // * * *
 
 // Checks if the file has the interpreter line.
-func checkInterpreter(fd *os.File) bool {
-	buf := bufio.NewReader(fd)
+func checkInterpreter(f *os.File) bool {
+	buf := bufio.NewReader(f)
 
 	firstLine, _, err := buf.ReadLine()
 	if err != nil {
@@ -251,18 +255,8 @@ func hash(filePath string) string {
 	return strconv.Uitoa(uint(crc))
 }
 
-// Opens the script in mode for reading and writing.
-func openFile(filename string) *os.File {
-	f, err := os.OpenFile(filename, os.O_RDWR, 0)
-	if err != nil {
-		fatalf("Could not open file: %s\n", err)
-	}
-
-	return f
-}
-
 // Executes the binary file.
-func run(binary string) {
+func runAndExit(binary string) {
 	cmd := exec.Command(binary)
 	cmd.Env = os.Environ()
 	cmd.Stdin = os.Stdin

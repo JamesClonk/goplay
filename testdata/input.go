00b8b1ee496118403f6f9adb948630e0e7db3ef4
Jonas mg <jonas@matx.cc>
2012-07-31 21:31:21 +0100

    Add tester

--- testdata/input.go
+++ testdata/input.go
@@ -13,12 +13,12 @@ func main() {
 	log.SetFlags(0)
 	log.SetPrefix("ERROR: ")
 
-	fmt.Printf("== Testing Stdin\n\nWrite and press Enter to finish: ")
+	fmt.Printf("(Write and press Enter to finish)\n")
 
 	in := bufio.NewReader(os.Stdin)
 	line, err := in.ReadString('\n')
 	if err != nil {
 		fmt.Println("ERROR: ", err)
 	}
-	fmt.Printf("Entered: %s", line)
+	fmt.Printf("%s", line)
 }
--- testdata/output.go
+++ testdata/output.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello!")
+	fmt.Println("The night is all magic")
 }

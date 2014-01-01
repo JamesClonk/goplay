5a11d3e665d49fad9f3d24286a813d728b1a46d2
jamesclonk@jamesclonk.ch <jamesclonk@jamesclonk.ch>
2014-01-01 13:28:44 +0100

    fixed travis-ci build script

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 export PATH=$PATH:$GOPATH/bin
+echo " "
+pathArray=$(echo $GOPATH | tr ":" "\n")
+for p in $pathArray
+do
+	echo "add $p/bin to PATH"
+    export PATH=$PATH:$p/bin
+done
+
+echo " "
+echo "PATH = $PATH"
+echo "GOPATH = $GOPATH"
+
 go install
-# travis-ci cannot find $GOPATH/bin/goplay
-#go test -v
+
+echo " "
+echo "look for goplay"
+which goplay
+
+echo " "
+echo "run tests"
+go test -v
+EXITCODE=$?
+
+exit $EXITCODE
